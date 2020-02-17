f9a52d192f3a285cbf4dc2b881796b031ae9d557
CristianoCarvalho86 <carvalho.m.cristiano@gmail.com>
2020-02-17 18:20:04 -0300

    Criação do logger

---
+++ Acelera.Logger/Logger.cs
@@ -0,0 +1,68 @@
+﻿using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acelera.Logger
+{
+    public class MyLogger
+    {
+        private string path;
+        private StreamWriter writer;
+        public MyLogger(string _path)
+        {
+            path = _path;
+            writer = File.CreateText(path);
+        }
+
+        public void Escrever(string texto)
+        {
+            writer.Write(texto);
+        }
+
+        public void InicioOperacao(OperacaoEnum operacao)
+        {
+            Escrever("Inicio da Operacao : " + operacao.GetEnumDescription());
+        }
+
+        public void SucessoDaOperacao(OperacaoEnum operacao)
+        {
+            Escrever($"Operacao : {operacao.GetEnumDescription()} --- Ok");
+        }
+
+        public void ErroNaOperacao(OperacaoEnum operacao)
+        {
+            Escrever($"Operacao : {operacao.GetEnumDescription()} --- Falha");
+        }
+
+        public void Erro(string descricao)
+        {
+            Escrever($"Houve um erro: {descricao}");
+        }
+
+        public void Erro(Exception ex)
+        {
+            Escrever($"Houve um erro: {ex.ToString()}");
+        }
+
+        public void LogComando(string comandoEnviado, string retornoComando)
+        {
+            Escrever($"Comando enviado : {comandoEnviado}");
+            Escrever($"Retorno {retornoComando}");
+        }
+
+        public void TesteSucesso()
+        {
+            Escrever($"Teste passado com sucesso");
+        }
+
+        public void TesteComFalha()
+        {
+            Escrever($"Teste com falha");
+        }
+    }
+}
--- Acelera.Testes/TesteBase.cs
+++ Acelera.Testes/TesteBase.cs
@@ -1,4 +1,7 @@
-﻿using Acelera.Testes.Adapters;
+﻿using Acelera.Domain.Enums;
+using Acelera.Logger;
+using Acelera.Testes.Adapters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -8,8 +11,10 @@ using System.Threading.Tasks;
 
 namespace Acelera.Testes
 {
+    [TestClass]
     public abstract class TesteBase
     {
+
         IntegracaoCMD integracao;
         private string pastaOrigem
         {
@@ -35,6 +40,56 @@ namespace Acelera.Testes
                 return origem;
             }
         }
+
+        private string comandoExecutar
+        {
+            get
+            {
+                return $"hdbsql -n {serverHana} -u {usuarioHana} -p {senhaHana} -e \"select host from m_database\"";
+            }
+        }
+
+        private string comandoSelect
+        {
+            get
+            {
+                return $"hdbsql -n {serverHana} -u {usuarioHana} -p {senhaHana} -e \"select host from m_database\"";
+            }
+        }
+
+        private string usuarioHana
+        {
+            get
+            {
+                var usuario = ConfigurationManager.AppSettings.Get("UsuarioHana");
+                if (string.IsNullOrEmpty(usuario))
+                    throw new Exception("Usuario Hana nao definido");
+                return usuario;
+            }
+        }
+
+        private string senhaHana
+        {
+            get
+            {
+                var senha = ConfigurationManager.AppSettings.Get("SenhaHana");
+                if (string.IsNullOrEmpty(senha))
+                    throw new Exception("Senha Hana nao definida");
+                return senha;
+            }
+        }
+
+        private string serverHana
+        {
+            get
+            {
+                var server = ConfigurationManager.AppSettings.Get("ServerHana");
+                if (string.IsNullOrEmpty(server))
+                    throw new Exception("Endereco do Servidor Hana nao definido");
+                return server;
+            }
+        }
+
         protected string ObterArquivoOrigem(string nomeArquivo)
         {
             return pastaOrigem + nomeArquivo;
@@ -45,11 +100,21 @@ namespace Acelera.Testes
             return pastaDestino + nomeArquivo;
         }
 
-        protected string ChamarExecucao()
+        protected void ChamarExecucao(MyLogger logger)
         {
-            integracao = new IntegracaoCMD();
-            integracao.AbrirCMD();
-            return integracao.ChamarAplicativo();
+            logger.InicioOperacao(OperacaoEnum.Processar);
+            try
+            {
+                integracao = new IntegracaoCMD();
+                integracao.AbrirCMD();
+                integracao.ChamarAplicativo();
+                logger.SucessoDaOperacao(OperacaoEnum.Processar);
+            }
+            catch (Exception ex)
+            {
+                logger.Erro(ex);
+                throw;
+            }
         }
 
         protected string ObterRetornoUltimoMetodo()
