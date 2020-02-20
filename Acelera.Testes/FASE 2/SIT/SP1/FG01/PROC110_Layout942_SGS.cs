f2487c3c97ba7cf095c29c167140034b8727b094
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-02-20 13:53:46 -0300

    Proc110

--- Acelera.Testes/FASE 2/SIT/SP1/FG01/PROC110_Layout942_SGS.cs
+++ Acelera.Testes/FASE 2/SIT/SP1/FG01/PROC110_Layout942_SGS.cs
@@ -5,26 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Acelera.Testes.FASE_2.SIT.SP1.FG00
+namespace Acelera.Testes.FASE_2.SIT.SP1.FG01
 {
     [TestClass]
-    public class PROC101_Layout942_SGS : TesteBase
+    public class PROC110_Layout942_SGS : TesteBase
     {
+
         /// <summary>
-        /// No arquivo SINISTRO repetir 2x o registro do Trailler, onde o TIPO REGISTRO é igual a 9
+        /// No arquivo LANCTO_COMISSAO repetir 3x o mesmo registro do Body onde o TIPO REGISTRO é igual a 03
         /// </summary>
         [TestMethod]
         [TestCategory("Com Critica")]
-        public void SAP_1197_SINISTRO_2xTrailler_2xHeader()
+        public void SAP_2281_SINISTRO_3xBody()
         {
         }
 
         /// <summary>
-        /// No arquivo SINISTRO apresentar somente um registro do Header
+        /// Importar arquivo s/ registro duplicado
         /// </summary>
         [TestMethod]
         [TestCategory("Sem Critica")]
-        public void SAP_1208_SINISTRO()
+        public void SAP_2455_SINISTRO()
         {
         }
 
---
+++ Acelera.Testes/FASE 2/SIT/SP1/FG01/PROC110_Layout94_TIM.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acelera.Testes.FASE_2.SIT.SP1.FG01
+{
+    [TestClass]
+    public class PROC110_Layout94_TIM : TesteBase
+    {
+
+        /// <summary>
+        /// No arquivo SINISTRO repetir 1x o mesmo registro do Body onde o TIPO REGISTRO é igual a 03
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_2300_SINISTRO_1xBody()
+        {
+        }
+
+        /// <summary>
+        /// No arquivo LANCTO_COMISSAO repetir 1x o mesmo registro do Body onde o TIPO REGISTRO é igual a 03
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_2299_LANCTO_COMISSAO_1xBody()
+        {
+        }
+
+        /// <summary>
+        /// No arquivo CLIENTE repetir 1x o mesmo registro do Body onde o TIPO REGISTRO é igual a 03
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_2295_CLIENTE_1xBody()
+        {
+        }
+
+        /// <summary>
+        /// No arquivo PARC_EMISSAO repetir 1x o mesmo registro do Body onde o TIPO REGISTRO é igual a 03
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_2296_PARC_EMISSAO_1xBody()
+        {
+        }
+
+        /// <summary>
+        /// No arquivo EMS_COMISSAO repetir 1x o mesmo registro do Body onde o TIPO REGISTRO é igual a 03
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_2297_EMS_COMISSAO_1xBody()
+        {
+        }
+
+        /// <summary>
+        /// No arquivo OCR_COBRANCA repetir 1x o mesmo registro do Body onde o TIPO REGISTRO é igual a 03
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_2298_OCR_COBRANCA_1xBody()
+        {
+        }
+
+        /// <summary>
+        /// Importar arquivo s/ registro duplicado
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_2443_CLIENTE()
+        {
+        }
+
+        /// <summary>
+        /// Importar arquivo s/ registro duplicado
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_2445_EMS_COMISSAO()
+        {
+        }
+
+        /// <summary>
+        /// Importar arquivo s/ registro duplicado
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_2446_OCR_COBRANCA()
+        {
+        }
+
+        /// <summary>
+        /// Importar arquivo s/ registro duplicado
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_2447_LANCTO_COMISSAO()
+        {
+        }
+
+        /// <summary>
+        /// Importar arquivo s/ registro duplicado
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_2448_SINISTRO()
+        {
+        }
+
+        /// <summary>
+        /// Importar arquivo s/ registro duplicado
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_2444_PARC_EMISSAO()
+        {
+        }
+
+    }
+}
