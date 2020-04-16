1364439bef7d49ce7bcb87fb36947d6836d37487
CristianoCarvalho86 <carvalho.m.cristiano@gmail.com>
2020-04-16 18:14:28 -0300

    Proc 70

---
+++ Acelera.Testes/FASE 2/SIT/SP2/FG02/PROC70_Layout94_LASA.cs
@@ -0,0 +1,77 @@
+﻿using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_3;
+using Acelera.Domain.Layouts._9_4;
+using Acelera.Domain.Layouts._9_4_2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Acelera.Testes.FASE_2.SIT.SP2.FG02
+{
+    [TestClass]
+    public class PROC70_Layout94_LASA : TestesFG02
+    {
+
+        /// <summary>
+        /// 	SINISTRO- Não informar CD_COBERTURA
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_4104_SEM_CD_COBERTURA()
+        {
+            IniciarTeste(TipoArquivo.Sinistro, "4104", "FG02 - PROC70 - SINISTRO- Não informar CD_COBERTURA");
+            arquivo = new Arquivo_Layout_9_4_Sinistro();
+            arquivo.Carregar(ObterArquivoOrigem("C01.LASA.SINISTRO-EV-3296-20200324.txt"));
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarLinha(0, "CD_COBERTURA","") ;
+
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo($"C01.LASA.SINISTRO-EV-/*R*/-20200324.txt");
+
+            //VALIDAR FG's ANTERIORES
+            ValidarFGsAnteriores();
+
+            //Executar FG02
+            ChamarExecucao(FG02_Tarefas.Sinistro.ObterTexto());
+
+            //VALIDAR NA FG02
+            ValidarLogProcessamento(true);
+            ValidarStages(CodigoStage.ReprovadoNegocioSemDependencia);
+            ValidarTabelaDeRetorno(1,"70");
+            ValidarTeste();
+
+        }
+        /// <summary>
+        /// SINISTRO- Não informar CD_COBERTURA
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_4105_CD_SINISTRO_MONTADO_OK()
+        {
+            IniciarTeste(TipoArquivo.Sinistro, "4105", "FG02 - PROC70 - SINISTRO- Não informar CD_COBERTURA");
+            arquivo = new Arquivo_Layout_9_4_Sinistro();
+            arquivo.Carregar(ObterArquivoOrigem("C01.LASA.SINISTRO-EV-3296-20200324.txt"));
+
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo($"C01.LASA.SINISTRO-EV-/*R*/-20200324.txt");
+
+            //VALIDAR FG's ANTERIORES
+            ValidarFGsAnteriores();
+
+            //Executar FG02
+            ChamarExecucao(FG02_Tarefas.Sinistro.ObterTexto());
+
+            //VALIDAR NA FG02
+            ValidarLogProcessamento(true);
+            ValidarStages(CodigoStage.AprovadoNegocioSemDependencia);
+            ValidarTabelaDeRetorno();
+            ValidarTeste();
+
+        }
+
+    }
+}
