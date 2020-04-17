2243754485acc37a5e5b56c1cb56525871b57c4f
CristianoCarvalho86 <carvalho.m.cristiano@gmail.com>
2020-04-17 17:41:37 -0300

    PROC 1002

---
+++ Acelera.Testes/FASE 2/SIT/SP2/FG02/PROC1002_Layout93_VIVO.cs
@@ -0,0 +1,47 @@
+﻿using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_3;
+using Acelera.Domain.Layouts._9_4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acelera.Testes.FASE_2.SIT.SP2.FG02
+{
+    [TestClass]
+    public class PROC1002_Layout93_VIVO : TestesFG02
+    {
+
+        /// <summary>
+        /// Informar no campo Cd_PRODUTO um produto parametrizado para o CD_TPA (CD_OPERAÇÃO) na tabela TAB_PRM_PRD_COBERTURA_7009
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_3801_PARCEMS_AUTO_SEM_CRITICA()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "3801", "FG02 - PROC1002 - PARCEMS- Informar no campo Cd_PRODUTO um produto não parametrizado para o CD_TPA (CD_OPERAÇÃO) na tabela TAB_PRM_PRD_COBERTURA_7009");
+
+            arquivo = new Arquivo_Layout_9_3_ParcEmissaoAuto();
+            arquivo.Carregar(ObterArquivoOrigem("C01.VIVO.PARCEMSAUTO-EV-1864-20200211.txt"));
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarLinha(1, "CD_PRODUTO", dados.ObterCdProdutoParaTPA(ObterValorHeader("CD_TPA"), false));
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //VALIDAR FG's ANTERIORES
+            ValidarFGsAnteriores();
+
+            //Executar FG02
+            ChamarExecucao(FG02_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NA FG01
+            ValidarLogProcessamento(true);
+            ValidarStages(CodigoStage.AprovadoNegocioSemDependencia);
+            ValidarTabelaDeRetorno();
+            ValidarTeste();
+
+        }
+
+    }
+}
---
+++ Acelera.Testes/FASE 2/SIT/SP2/FG02/PROC1002_Layout94_Softbox.cs
@@ -0,0 +1,78 @@
+﻿using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_3;
+using Acelera.Domain.Layouts._9_4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acelera.Testes.FASE_2.SIT.SP2.FG02
+{
+    [TestClass]
+    public class PROC1002_Layout94_Softbox : TestesFG02
+    {
+        /// <summary>
+        /// Informar no campo Cd_PRODUTO um produto não parametrizado para o CD_TPA (CD_OPERAÇÃO) na tabela TAB_PRM_PRD_COBERTURA_7009
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_3799_PARCEMS_PRODUTO_NAO_CADASTRADO_PARA_OPERACAO()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "3799", "FG02 - PROC1002 - PARCEMS- Informar no campo Cd_PRODUTO um produto não parametrizado para o CD_TPA (CD_OPERAÇÃO) na tabela TAB_PRM_PRD_COBERTURA_7009");
+
+            arquivo = new Arquivo_Layout_9_4_ParcEmissao();
+            arquivo.Carregar(ObterArquivoOrigem("C01.SOFTBOX.PARCEMS-EV-3308-20200325.txt"));
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarLinha(1, "CD_PRODUTO", dados.ObterCdProdutoParaTPA(ObterValorHeader("CD_TPA"),false));
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //VALIDAR FG's ANTERIORES
+            ValidarFGsAnteriores();
+
+            //Executar FG02
+            ChamarExecucao(FG02_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NA FG01
+            ValidarLogProcessamento(true);
+            ValidarStages(CodigoStage.ReprovadoNegocioSemDependencia);
+            ValidarTabelaDeRetorno(1, "1002");
+            ValidarTeste();
+
+        }
+
+        /// <summary>
+        /// Informar no campo Cd_PRODUTO um produto parametrizado para o CD_TPA (CD_OPERAÇÃO) na tabela TAB_PRM_PRD_COBERTURA_7009
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_3800_PARCEMS_PRODUTO_NAO_CADASTRADO_PARA_OPERACAO()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "3800", "FG02 - PROC1002 - PARCEMS- Informar no campo Cd_PRODUTO um produto não parametrizado para o CD_TPA (CD_OPERAÇÃO) na tabela TAB_PRM_PRD_COBERTURA_7009");
+
+            arquivo = new Arquivo_Layout_9_4_ParcEmissao();
+            arquivo.Carregar(ObterArquivoOrigem("C01.SOFTBOX.PARCEMS-EV-3308-20200325.txt"));
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarLinha(1, "CD_PRODUTO", dados.ObterCdProdutoParaTPA(ObterValorHeader("CD_TPA"), true));
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //VALIDAR FG's ANTERIORES
+            ValidarFGsAnteriores();
+
+            //Executar FG02
+            ChamarExecucao(FG02_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NA FG01
+            ValidarLogProcessamento(true);
+            ValidarStages(CodigoStage.AprovadoNegocioSemDependencia);
+            ValidarTabelaDeRetorno();
+            ValidarTeste();
+
+        }
+
+    }
+}
