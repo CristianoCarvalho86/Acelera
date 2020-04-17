2b8c3137f7e7653325993754388c0b6eab830cd2
CristianoCarvalho86 <carvalho.m.cristiano@gmail.com>
2020-04-17 15:51:10 -0300

    Valor Formatado

---
+++ Acelera.Domain/Extensions/DecimalExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acelera.Domain.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static string ValorFormatado(this decimal numero)
+        {
+            return numero.ToString().Replace(',', '.');
+        }
+    }
+}
