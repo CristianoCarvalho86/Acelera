2b87cd026ea2311974bf1530800dfded1d5f5fe7
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-05-05 11:08:49 -0300

    .

---
+++ Acelera.Testes/FASE 2/SIT/SP3/FG05/PROC22/PROC22_Layout93_LASA.cs
@@ -0,0 +1,33 @@
+﻿using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_3;
+using Acelera.Domain.Layouts._9_4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acelera.Testes.FASE_2.SIT.SP3.FG05.PROC22
+{
+    [TestClass]
+    public class PROC22_Layout94_LASA : TestesFG05
+    {
+
+        /// <summary>
+        /// Gerar um arquivo anteriormente que popule a tabela ODS que não possua nenhum item a ser criticado. 
+        /// Em outro arquivo, enviar as mesmas informações do arquivo anterior nos campos da tabela TAB_PRM_LAYOUT_7016 para o NM_TIPO_ARQUIVO= SINISTRO, CD_VERSAO_ARQUIVO=9.4, TP_REGISTRO=3 e ID_PRIMARY_KEY=1.
+        /// Todos os campos exibidos nessa consulta na coluna NM_ATRIBUTO_LAYOUT devem ser iguais aos do primeiro arquivo.
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Comm Critica")]
+        public void SAP_()
+        {
+            IniciarTeste(TipoArquivo.Sinistro, "3801", "FG02 - PROC1002 - ");
+
+            arquivo = new Arquivo_Layout_9_3_ParcEmissaoAuto();
+            CarregarArquivo(arquivo ,1 , OperadoraEnum.VIVO);
+
+
+
+        }
+
+    }
+}
