a2e789ecdcd3678703cf7c099d2854a28d18bf9f
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-06-26 10:33:48 -0300

    fg09

---
+++ Acelera.Testes/FASE 2/SIT/SP4/FG09/PROC42_LAYOUT94_LASA.cs
@@ -0,0 +1,61 @@
+﻿using Acelera.Data;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts;
+using Acelera.Domain.Layouts._9_3;
+using Acelera.Domain.Layouts._9_4;
+using Acelera.Domain.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acelera.Testes.FASE_2.SIT.SP4.FG09
+{
+    [TestClass]
+    public class PROC42_LAYOUT94_LASA : TestesFG09
+    {
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_5284()
+        {
+            IniciarTeste(TipoArquivo.Sinistro, "5284", "FG09 - PROC42 - ");
+
+            //Envia parc normal
+            arquivo = new Arquivo_Layout_9_4_ParcEmissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.LASA);
+
+            AlterarLinha(0, "CD_TIPO_EMISSAO", "20");
+            AlterarLinha(0, "NR_SEQUENCIAL_EMISSAO", "1");
+            AlterarLinha(0, "NR_ENDOSSO", "0");
+            var idCanc = arquivo.ObterValorFormatadoSeExistirCampo(0, "ID_TRANSACAO");
+
+            EnviarParaOds(arquivo);
+            var arquivoods1 = arquivo.Clone();
+
+            //Envia Parc com id cancelamento igual id transição do anterior
+            arquivo = new Arquivo_Layout_9_4_ParcEmissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.LASA);
+
+            EnviarCancelamento(arquivoods1.ObterLinha(0), "10");
+            
+
+            EnviarParaOds(arquivo);
+            var arquivoods2 = arquivo.Clone();
+
+            //Sinistro referente a cancelamento
+            arquivo = new Arquivo_Layout_9_4_Sinistro();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.LASA);
+
+            EnviarCancelamento(arquivoods2.ObterLinha(0), "10");
+
+            SalvarArquivo();
+
+            ExecutarEValidar(CodigoStage.ReprovadoNegocioComDependencia, "181", 1);
+
+        }
+
+    }
+}
