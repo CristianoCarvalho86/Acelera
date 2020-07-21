eb7dd0a7a1125599f8da2a798e714ebbd08fe148
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-07-21 15:13:41 -0300

    .

---
+++ Acelera.Testes/FASE 2/SIT/SP1/FG00/PROC95_Layout96_PAPCARD.cs
@@ -0,0 +1,372 @@
+﻿using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acelera.Testes.FASE_2.SIT.SP1.FG00
+{
+    [TestClass]
+    public class PROC95_Layout96_PAPCARD : TestesFG00
+    {
+        /// <summary>
+        /// Não informar o registro do Header no arquivo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7151_COMISSAO_SemHeader()
+        {
+            IniciarTeste(TipoArquivo.Comissao, "1266", " FG00 - PROC95 - Não informar o registro do Header no arquivo");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_EmsComissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            RemoverHeader();
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.Comissao.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// Não informar o registro do Header no arquivo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7154_SINISTRO_SemHeader()
+        {
+            IniciarTeste(TipoArquivo.Sinistro, "1269", "FG00 - PROC95 - Não informar o registro do Header no arquivo");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_Sinistro();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            RemoverHeader();
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.Sinistro.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// Não informar o registro do Header no arquivo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7152_OCR_COBRANCA_SemHeader()
+        {
+            IniciarTeste(TipoArquivo.OCRCobranca, "1267", "FG00 - PROC95 - Não informar o registro do Header no arquivo");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_OcrCobranca();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            RemoverHeader();
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.OCRCobranca.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// Não informar o registro do Header no arquivo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7150_PARC_EMISSAO_SemHeader()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "7150", "FG00 - PROC95 - Não informar o registro do Header no arquivo");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_ParcEmissaoAuto();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            RemoverHeader();
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// Não informar o registro do Header no arquivo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7149_CLIENTE_SemHeader()
+        {
+            IniciarTeste(TipoArquivo.Cliente, "1264", "FG00 - PROC95 - Não informar o registro do Header no arquivo");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_Cliente();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            RemoverHeader();
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.Cliente.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// No Header do arquivo EMS_COMISSAO no campo TIPO_REGISTRO, não informar valor, campo em branco, respeitando a tamanho do campo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7157_EMS_COMISSAO_SemTipoRegistro()
+        {
+            IniciarTeste(TipoArquivo.Comissao, "1260", "FG00 - PROC95 -  No Header do arquivo EMS_COMISSAO no campo TIPO_REGISTRO, não informar valor");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_EmsComissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarHeader("TIPO_REGISTRO", "");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.Comissao.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// No Header do arquivo SINISTRO no campo TIPO_REGISTRO, não informar valor, campo em branco, respeitando a tamanho do campo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7160_EMS_SINISTRO_SemTipoRegistro()
+        {
+            IniciarTeste(TipoArquivo.Sinistro, "1263", "FG00 - PROC95 - No Header do arquivo SINISTRO no campo TIPO_REGISTRO, não informar valor");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_Sinistro();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarHeader("TIPO_REGISTRO", "");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.Sinistro.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// No Header do arquivo OCR_COBRANCA no campo TIPO_REGISTRO, não informar valor, campo em branco, respeitando a tamanho do campo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7158_OCR_COBRANCA_SemTipoRegistro()
+        {
+            IniciarTeste(TipoArquivo.OCRCobranca, "1261", "FG00 - PROC95 - No Header do arquivo OCR_COBRANCA no campo TIPO_REGISTRO, não informar valor");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_OcrCobranca();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarHeader("TIPO_REGISTRO", "");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.OCRCobranca.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// No Header do arquivo PARC_EMISSAO no campo TIPO_REGISTRO, não informar valor, campo em branco, respeitando a tamanho do campopo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7156_PARC_EMISSAO_SemTipoRegistro()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "1259", "FG00 - PROC95 - No Header do arquivo PARC_EMISSAO no campo TIPO_REGISTRO, não informar valor");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_ParcEmissaoAuto();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarHeader("TIPO_REGISTRO", "");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// No Header do arquivo CLIENTE no campo TIPO_REGISTRO, não informar valor, campo em branco, respeitando a tamanho do campo
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_7155_CLIENTE_SemTipoRegistro()
+        {
+            IniciarTeste(TipoArquivo.Cliente, "1258", "FG00 - PROC95 - No Header do arquivo CLIENTE no campo TIPO_REGISTRO, não informar valor");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_6_Cliente();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarHeader("TIPO_REGISTRO", "");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //PROCESSAR O ARQUIVO CRIADO
+            ChamarExecucao(FG00_Tarefas.Cliente.ObterTexto());
+
+            //VALIDAR NO BANCO A ALTERACAO
+            ValidarLogProcessamento(true);
+            ValidarControleArquivo("Estrutura de header (01) nao encontrada");
+            ValidarTabelaDeRetorno("95");
+            ValidarStages(false);
+            ValidarTeste();
+        }
+
+        /// <summary>
+        /// No Header do arquivo CLIENTE no campo TIPO_REGISTRO informar código 01
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        [TestCategory("Semm Critica")]
+        public void SAP_1330_CLIENTE_TipoRegistro01()
+        {
+        }
+
+        /// <summary>
+        /// No Header do arquivo PARC_EMISSAO no campo TIPO_REGISTRO informar código 01
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        [TestCategory("Semm Critica")]
+        public void SAP_1331_PARC_EMISSAO_TipoRegistro01()
+        {
+        }
+
+        /// <summary>
+        /// No Header do arquivo EMS_COMISSAO no campo TIPO_REGISTRO informar código 01
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        [TestCategory("Semm Critica")]
+        public void SAP_1332_EMS_COMISSAO_TipoRegistro01()
+        {
+        }
+
+        /// <summary>
+        /// No Header do arquivo OCR_COBRANCA no campo TIPO_REGISTRO informar código 01
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        [TestCategory("Semm Critica")]
+        public void SAP_1333_OCR_COBRANCA_TipoRegistro01()
+        {
+        }
+
+        /// <summary>
+        /// No Header do arquivo LANCTO_COMISSAO no campo TIPO_REGISTRO informar código 01
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        [TestCategory("Semm Critica")]
+        public void SAP_1334_LANCTO_COMISSAO_TipoRegistro01()
+        {
+        }
+
+        /// <summary>
+        /// No Header do arquivo SINISTRO no campo TIPO_REGISTRO informar código 01
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        [TestCategory("Semm Critica")]
+        public void SAP_1335_SINISTRO_TipoRegistro01()
+        {
+        }
+    }
+}
---
+++ Acelera.Testes/FASE 2/SIT/SP1/FG01/PROC55_Layout94_PAPCARD.cs
@@ -0,0 +1,81 @@
+﻿using Acelera.Domain.Entidades;
+using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acelera.Testes.FASE_2.SIT.SP1.FG01
+{
+    [TestClass]
+    public class PRO55_Layout94_PAPCARD : TestesFG01
+    {
+
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_8913()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "8917", "Corretor com código SUSEP nulo");
+            arquivo = new Arquivo_Layout_9_4_EmsComissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarLinha(0, "CD_CORRETOR","");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //VALIDAR NA FG00
+            ValidarFGsAnteriores();
+
+            //Executar FG01
+            ChamarExecucao(FG01_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NA FG01
+            ValidarLogProcessamento(true);
+            ValidarStages(CodigoStage.RecusadoNaFG01);
+            ValidarTabelaDeRetorno("163");
+            ValidarTeste();
+        }
+
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_8912()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "8918", "Corretor com código SUSEP invalido");
+            arquivo = new Arquivo_Layout_9_4_EmsComissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarLinha(0, "CD_CORRETOR", "");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //VALIDAR NA FG00
+            ValidarFGsAnteriores();
+
+            //Executar FG01
+            ChamarExecucao(FG01_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NA FG01
+            ValidarLogProcessamento(true);
+            ValidarStages(CodigoStage.RecusadoNaFG01);
+            ValidarTabelaDeRetorno("163");
+            ValidarTeste();
+        }
+
+
+
+        /// <summary>
+        /// Importar arquivo com ID_TRANSACAO_CANC correto
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_2547_PARC_EMISSAO_ID_TRANSACAO_CANC()
+        {
+        }
+
+    }
+}
