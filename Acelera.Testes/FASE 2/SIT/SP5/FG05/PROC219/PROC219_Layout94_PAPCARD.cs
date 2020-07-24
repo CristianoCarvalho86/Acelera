ee2d242ced9ab5de731a3fce4fb8a5dbf882e771
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-07-24 14:40:35 -0300

    .

---
+++ Acelera.Testes/FASE 2/SIT/SP5/FG05/PROC219/PROC219_Layout94_PAPCARD.cs
@@ -0,0 +1,36 @@
+﻿using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_3;
+using Acelera.Domain.Layouts._9_6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acelera.Testes.FASE_2.SIT.SP5.FG05.PROC219
+{
+    [TestClass]
+    public class PROC219_Layout94_PAPCARD : TestesFG05
+    {
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_9224()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "9180", "FG05 - PROC219 - ");
+            
+
+            //Envia parc normal
+            var arquivoods1 = new Arquivo_Layout_9_6_ParcEmissao();
+            CarregarArquivo(arquivoods1, 1, OperadoraEnum.PAPCARD);
+
+            CriarNovoContrato(0);
+
+            ReplicarLinha(0, 1);
+            AlterarLinha(1, "CD_CLIENTE", "");
+            AlterarLinha(1, "NR_ENDOSSO", "");
+            AlterarLinha(1, "NR_SEQUENCIAL_EMISSAO", "");
+
+            SalvarArquivo();
+
+            ExecutarEValidar(CodigoStage.AprovadoNegocioComDependencia);
+        }
+    }
+}
---
+++ Acelera.Testes/FASE 2/SIT/SP5/FG05/PROC225/PROC225_Layout94_PAPCARD.cs
@@ -0,0 +1,89 @@
+﻿using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_3;
+using Acelera.Domain.Layouts._9_6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acelera.Testes.FASE_2.SIT.SP5.FG05.PROC225
+{
+    [TestClass]
+    public class PROC225_Layout94_PAPCARD : TestesFG05
+    {
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_9267()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "9267", "FG05 - PROC219 - ");
+
+
+            //Envia parc normal
+            var arquivoods1 = new Arquivo_Layout_9_6_ParcEmissao();
+            CarregarArquivo(arquivoods1, 2, OperadoraEnum.PAPCARD);
+
+            CriarNovoContrato(0);
+            var campos = new string[] 
+            { "CD_CONTRATO", "NR_APOLICE", "NR_PROPOSTA", "ID_TRANSACAO", "CD_COBERTURA","NR_SEQ_EMISSAO","TIPO_EMISSAO", "NR_ENDOSSO" };
+
+            IgualarCampos(arquivoods1.ObterLinha(0), arquivoods1.ObterLinha(1), campos);
+
+            SalvarArquivo();
+
+            ExecutarEValidar(CodigoStage.ReprovadoNegocioComDependencia, "219", 1);
+        }
+
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_9268()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "9180", "FG05 - PROC46 - ");
+
+            //Envia parc normal
+            var arquivoods1 = new Arquivo_Layout_9_6_ParcEmissao();
+            CarregarArquivo(arquivoods1, 1, OperadoraEnum.PAPCARD);
+
+            CriarNovoContrato(0);
+            //arquivoods1.AlterarLinha(0, "CD_TIPO_EMISSAO", "1");
+            //arquivoods1.AlterarLinha(0, "NR_SEQUENCIAL_EMISSAO", "1");
+            //arquivoods1.AlterarLinha(0, "NR_ENDOSSO", "0");
+
+            EnviarParaOds(arquivoods1);
+
+            IniciarTeste(TipoArquivo.ParcEmissao, "9180", "FG05 - PROC46 - ");
+
+            //Envia parc normal
+            arquivo = new Arquivo_Layout_9_6_ParcEmissao();
+            CarregarArquivo(arquivoods1, 1, OperadoraEnum.PAPCARD);
+            var campos = new string[]
+            { "CD_CONTRATO", "NR_APOLICE", "NR_PROPOSTA", "ID_TRANSACAO", "CD_COBERTURA","NR_SEQ_EMISSAO","TIPO_EMISSAO", "NR_ENDOSSO" };
+
+            IgualarCampos(arquivoods1.ObterLinha(0), arquivo.ObterLinha(0), campos);
+
+            SalvarArquivo();
+
+            ExecutarEValidar(CodigoStage.ReprovadoNegocioComDependencia, "46", 1);
+        }
+
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_9184()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "9267", "FG05 - PROC219 - ");
+
+
+            //Envia parc normal
+            var arquivoods1 = new Arquivo_Layout_9_6_ParcEmissao();
+            CarregarArquivo(arquivoods1, 2, OperadoraEnum.PAPCARD);
+
+            CriarNovoContrato(0);
+            CriarNovoContrato(1);
+            var campos = new string[]
+            { "CD_COBERTURA","NR_SEQ_EMISSAO","TIPO_EMISSAO", "NR_ENDOSSO" };
+            IgualarCampos(arquivoods1.ObterLinha(0), arquivoods1.ObterLinha(1), campos);
+
+            SalvarArquivo();
+
+            ExecutarEValidar(CodigoStage.AprovadoNegocioComDependencia);
+        }
+    }
+}
