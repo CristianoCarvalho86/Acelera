9bc8dd6cbf04aab0a29a89f28d16fbd63016b59e
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-07-28 11:05:59 -0300

    Proc235

--- Acelera.Testes/FASE 2/SIT/SP5/FG05/PROC235/PROC235_Layout96_COOP.cs
+++ Acelera.Testes/FASE 2/SIT/SP5/FG05/PROC235/PROC235_Layout96_COOP.cs
@@ -35,6 +35,31 @@ namespace Acelera.Testes.FASE_2.SIT.SP5.FG05.PROC235
             ExecutarEValidar(CodigoStage.ReprovadoNegocioComDependencia, "235", 1);
         }
 
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_9369()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "9369", "SAP-9368:FG05 - PROC 235 - C/C - PARCELA - Contrato com registro rejeitado - Mesmo arquivo");
+            //Envia parc normal
+            AlterarCobertura(false);
+
+            arquivo = new Arquivo_Layout_9_4_ParcEmissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.TIM);
+
+            AlterarLinhaParaPrimeiraEmissao(arquivo, 0);
+            AdicionarNovaCoberturaNaEmissao(arquivo, dados);
+            AlterarLinha(1, "CD_RAMO", dados.ObterRamoRelacionadoACoberturaDiferenteDe(arquivo[1]["CD_COBERTURA"], arquivo[0]["CD_RAMO"], out string produto));
+            AlterarLinha(1, "CD_PRODUTO", produto);
+
+            SalvarArquivo();
+            var arquivoparc = arquivo.Clone();
+
+            CriarComissao<Arquivo_Layout_9_4_EmsComissao>(OperadoraEnum.COOP, arquivoparc);
+            AlterarLinha(0, "CD_RAMO", arquivoparc[1]["CD_RAMO"]);
+            AlterarLinha(1, "CD_RAMO", arquivoparc[0]["CD_RAMO"]);
+
+            ExecutarEValidar(CodigoStage.ReprovadoNegocioComDependencia, "235", 1);
+        }
 
     }
 }
