07ab9b02e670ece7793948ba7457a5a16be17c4d
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-07-28 11:09:12 -0300

    .

--- Acelera.Testes/FASE 2/SIT/SP5/FG05/PROC235/PROC235_Layout96_PAPCARD.cs
+++ Acelera.Testes/FASE 2/SIT/SP5/FG05/PROC235/PROC235_Layout96_PAPCARD.cs
@@ -11,28 +11,33 @@ using System.Threading.Tasks;
 namespace Acelera.Testes.FASE_2.SIT.SP5.FG05.PROC235
 {
     [TestClass]
-    public class PROC235_Layout96_COOP : TestesFG05
+    public class PROC235_Layout96_PAPCARD : TestesFG05
     {
         [TestMethod]
         [TestCategory("Com Critica")]
-        public void SAP_9368()
+        public void SAP_9372()
         {
-            IniciarTeste(TipoArquivo.ParcEmissao, "9368", "SAP-9368:FG05 - PROC 235 - C/C - PARCELA - Contrato com registro rejeitado - Mesmo arquivo");
+            IniciarTeste(TipoArquivo.ParcEmissao, "9372", "SAP-9368:FG05 - PROC 235 - C/C - PARCELA - Contrato com registro rejeitado - Mesmo arquivo");
             //Envia parc normal
+            AlterarCobertura(false);
+
             arquivo = new Arquivo_Layout_9_4_ParcEmissao();
             CarregarArquivo(arquivo, 1, OperadoraEnum.TIM);
 
-            var cobertura = dados.ObterCoberturaSimples(ObterValorHeader("CD_TPA"));
             AlterarLinhaParaPrimeiraEmissao(arquivo, 0);
-            AlterarLinha(1, "CD_RAMO", cobertura.CdRamo);
+            AdicionarNovaCoberturaNaEmissao(arquivo, dados);
+            AlterarLinha(1, "CD_RAMO", dados.ObterRamoRelacionadoACoberturaDiferenteDe(arquivo[1]["CD_COBERTURA"], arquivo[0]["CD_RAMO"], out string produto));
+            AlterarLinha(1, "CD_PRODUTO", produto);
 
             SalvarArquivo();
             var arquivoparc = arquivo.Clone();
 
-            CriarComissao<Arquivo_Layout_9_4_EmsComissao>(OperadoraEnum.COOP, arquivoparc);
-            AlterarLinha(1, "CD_RAMO", dados.ObterRamoRelacionadoACoberturaDiferenteDe(arquivo[0]["CD_COBERTURA"], arquivo[0]["CD_RAMO"], out string produto));
+            CriarComissao<Arquivo_Layout_9_4_EmsComissao>(OperadoraEnum.PAPCARD, arquivoparc);
+            AlterarLinha(0, "CD_RAMO", arquivoparc[1]["CD_RAMO"]);
+            AlterarLinha(1, "CD_RAMO", arquivoparc[0]["CD_RAMO"]);
 
             ExecutarEValidar(CodigoStage.ReprovadoNegocioComDependencia, "235", 1);
         }
+
     }
 }
