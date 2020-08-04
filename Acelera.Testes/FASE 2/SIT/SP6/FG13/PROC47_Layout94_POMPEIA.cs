e4a4b0f5d241424ca5c84eb5f33c7ad367f0d2a1
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-08-04 18:02:03 -0300

    .

--- Acelera.Testes/FASE 2/SIT/SP6/FG13/PROC47_Layout94_POMPEIA.cs
+++ Acelera.Testes/FASE 2/SIT/SP6/FG13/PROC47_Layout94_POMPEIA.cs
@@ -19,7 +19,7 @@ namespace Acelera.Testes.FASE_2.SIT.SP6.FG13.PROC47
         {
             IniciarTeste("9574", "", OperadoraEnum.POMPEIA);
 
-            SalvaExecutaEValida(true);
+            SalvaExecutaEValidaTrinca(true);
 
             arquivo = new Arquivo_Layout_9_4_OcrCobranca();
             CarregarArquivo(arquivo, 1, OperadoraEnum.POMPEIA);
@@ -30,7 +30,7 @@ namespace Acelera.Testes.FASE_2.SIT.SP6.FG13.PROC47
 
             SalvarArquivo();
 
-
+            ExecutarEValidarAteFG13(arquivo, CodigoStage.AprovadoNegocioSemDependencia);
 
         }
 
