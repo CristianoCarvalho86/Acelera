971e55d23198cea7dc2b38b425ea281dbee50ee6
Caique Rodrigues da Silva de Oliveira <caique.rodrigues@t2mlab.com>
2020-08-19 10:36:46 -0300

    .

---
+++ Acelera.Testes/FASE 2/SIT/SP5/FG02/PrioridadeB/PROC13/PROC13_Layout94_PAPCARD.cs
@@ -0,0 +1,79 @@
+﻿using Acelera.Domain.Entidades.Stages;
+using Acelera.Domain.Enums;
+using Acelera.Domain.Extensions;
+using Acelera.Domain.Layouts._9_4;
+using Acelera.Domain.Layouts._9_6;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acelera.Testes.FASE_2.SIT.SP5.FG02.PrioridadeB.PROC13
+{
+    [TestClass]
+    public class PROC13_Layout94_PAPCARD : TestesFG02
+    {
+
+        /// <summary>
+        /// Informar no arquivo PARC_EMISSAO_AUTO o campo VL_IS = 0
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Com Critica")]
+        public void SAP_2667_PARC_EMISSAO_VL_IS_negativo()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "2667", "FG02 - PROC13 - Informar no arquivo PARC_EMISSAO_AUTO o campo VL_IS = 0");
+            arquivo = new Arquivo_Layout_9_4_2_new_ParcEmissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+            AlterarLayout<Arquivo_Layout_9_6_ParcEmissao>(ref arquivo);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarLinha(3, "VL_IS", "0");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //VALIDAR FG's ANTERIORES
+            ValidarFGsAnteriores();
+
+            //Executar FG02
+            ChamarExecucao(FG02_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NA FG02
+            ValidarLogProcessamento(true);
+            ValidarStages(CodigoStage.ReprovadoNegocioSemDependencia);
+            ValidarTabelaDeRetorno(1, "13");
+            ValidarTeste();
+
+        }
+
+        /// <summary>
+        /// Informar no arquivo PARC_EMISSAO_AUTO o campo VL_IS=1
+        /// </summary>
+        [TestMethod]
+        [TestCategory("Sem Critica")]
+        public void SAP_2668_PARC_EMISSAO_semcritica()
+        {
+            IniciarTeste(TipoArquivo.ParcEmissao, "2668", "FG00 - PROC13 - Informar no arquivo PARC_EMISSAO_AUTO o campo VL_IS=1");
+
+            //CARREGAR O ARQUIVO BASE
+            arquivo = new Arquivo_Layout_9_4_2_new_ParcEmissao();
+            CarregarArquivo(arquivo, 1, OperadoraEnum.PAPCARD);
+            AlterarLayout<Arquivo_Layout_9_6_ParcEmissao>(ref arquivo);
+
+            //ALTERAR O VALOR SELECIONADO
+            AlterarLinha(3, "VL_IS", "1");
+
+            //SALVAR O NOVO ARQUIVO ALTERADO
+            SalvarArquivo();
+
+            //VALIDAR FG's ANTERIORES
+            ValidarFGsAnteriores();
+
+            //Executar FG02
+            ChamarExecucao(FG02_Tarefas.ParcEmissao.ObterTexto());
+
+            //VALIDAR NA FG01
+            ValidarLogProcessamento(true);
+            ValidarTabelaDeRetorno(false);
+            ValidarStages(CodigoStage.AprovadoNegocioSemDependencia);
+            ValidarTeste();
+        }
+    }
+}
