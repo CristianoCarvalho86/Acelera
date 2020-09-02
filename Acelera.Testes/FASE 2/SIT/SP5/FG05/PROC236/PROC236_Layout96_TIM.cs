0b7fbff47601c7bb5701f73ab604fde62eb87659
CristianoCarvalho86 <carvalho.m.cristiano@gmail.com>
2020-09-02 11:04:27 -0300

    abc

--- Acelera.Testes/FASE 2/SIT/SP5/FG05/PROC236/PROC236_Layout96_TIM.cs
+++ Acelera.Testes/FASE 2/SIT/SP5/FG05/PROC236/PROC236_Layout96_TIM.cs
@@ -56,8 +56,7 @@ namespace Acelera.Testes.FASE_2.SIT.SP5.FG05.PROC236
             AlterarLinha(0, "DT_FIM_VIGENCIA", SomarData(arquivo[0]["DT_FIM_VIGENCIA"], 30));
             AlterarLinha(1, "DT_INICIO_VIGENCIA", SomarData(arquivo[0]["DT_INICIO_VIGENCIA"], 30));
             AlterarLinha(1, "DT_FIM_VIGENCIA", SomarData(arquivo[0]["DT_FIM_VIGENCIA"], 30));
-            AlterarLinha(0, "NR_SEQUENCIAL_EMISSAO", "3");
-            AlterarLinha(1, "NR_SEQUENCIAL_EMISSAO", "4");
+
 
             SelecionarLinhaParaValidacao(0);
             SalvarArquivo();
