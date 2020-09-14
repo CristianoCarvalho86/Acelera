878c5821259ee0ef4569259696fb885ad0b753c0
CristianoCarvalho86 <carvalho.m.cristiano@gmail.com>
2020-09-14 10:37:22 -0300

    abc

--- Acelera.Testes/DataAccessRep/ODS/ODSUpdateParcCancelamento.cs
+++ Acelera.Testes/DataAccessRep/ODS/ODSUpdateParcCancelamento.cs
@@ -13,15 +13,21 @@ namespace Acelera.Testes.DataAccessRep.ODS
     {
         public static string UpdateParcOdsText()
         {
-            return $"update a " +
-            " set a.DT_CANCELAMENTO = b.dt_emissao, " +
-            " a.CD_PARCELA_CANCELAMENTO = b.cd_parcela, " +
-            " a.CD_STATUS_PARCELA = 'CA' " +
-            $" from {Parametros.instanciaDB}.tab_ods_parcela_2003 a " +
-            " inner " +
-            $" join (select id_transacao_canc, dt_emissao, cd_parcela from {Parametros.instanciaDB}.tab_ods_parcela_2003 " +
-            " where id_transacao_canc is not null) b on " +
-            " a.id_transacao = b.id_transacao_canc;";
+            return $"update a" +
+                    $" set a.DT_CANCELAMENTO = b.dt_emissao,a.CD_PARCELA_CANCELAMENTO = b.cd_parcela,a.CD_STATUS_PARCELA = 'CA'" +
+                    $" from {Parametros.instanciaDB}.tab_ods_parcela_2003 a" +
+                    $" inner " +
+                    $" join TAB_ODS_COBERTURA_2005 c " +
+                    $" on a.cd_parcela = c.cd_parcela" +
+                    $" inner " +
+                    $" join (select id_transacao_canc, dt_emissao, a1.cd_parcela from {Parametros.instanciaDB}.tab_ods_parcela_2003 a1 " +
+                    $" inner join {Parametros.instanciaDB}.TAB_ODS_COBERTURA_2005 b1" +
+                    $" on a1.cd_parcela = b1.cd_parcela" +
+                    $" where id_transacao_canc is not null) b" +
+                    $" on c.id_transacao = b.id_transacao_canc" +
+                    $" and cd_status_parcela = 'EM'" +
+                    $" and dt_cancelamento is null " +
+                    $" and cd_parcela_cancelamento is null;";
         }
 
         public static string UpdateCmsOdsText()
